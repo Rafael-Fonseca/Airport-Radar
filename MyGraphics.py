8fbc1d8ef81a0f4e8ff0ae9f1d2e8f14c747d7d3
Rafael-Fonseca <rafael.abreu.unb@gmail.com>
2020-02-19 18:25:17 -0300

    Update MyGraphics.py method line
    Realizado alteração que permite ao código imprimir linhas com deltaX < 0 e deltaY > 0 .
    
    O código agora é capaz de imprimir linhas com deltaX positivo ou negativo.
    
    Ainda se faz necessário realizar as impressões das linhas em que deltaY > deltaX.

--- MyGraphics.py
+++ MyGraphics.py
@@ -50,10 +50,18 @@ class MyGraphics:
 
             #TODO: Refatorar, substituir os if(s) por um dicionário de funções.
 
-    def line(self, initial_x, final_x, initial_y, final_y, color, thickness, type_line):
+    def line(self, initial_x, initial_y, final_x, final_y, color, thickness, type_line):
 
         delta_x = final_x - initial_x
         delta_y = final_y - initial_y
+
+        if delta_x < 0 and delta_y > 0:
+            initial_x, final_x = final_x, initial_x
+            initial_y, final_y = final_y, initial_y
+
+            delta_x = final_x - initial_x
+            delta_y = final_y - initial_y
+
         y_increment = 1
 
         if delta_y < 0:
@@ -63,21 +71,30 @@ class MyGraphics:
         incremental_error = 2 * delta_y - delta_x
         y = initial_y
 
-        # TODO: Estrutura de decisão baseada no crescimento positivo ou negativo de X ou Y, conforme o caso, seleciona qual
-        # TODO: linha do For deve ser selecionada.
-        # for x in range(x0, x1-1, -1):
-        for x in range(initial_x, final_x + 1):
-            self.point(x, y, color, thickness)
-            print('to no for')  #Esse print auxilia enquanto o TODO acima não for realizado.
-            if incremental_error > 0:
-                y = y + y_increment
-                incremental_error = incremental_error - 2 * delta_x
+        if delta_x < 0:
+            for x in range(initial_x, final_x-1, -1):
+                self.point(x, y, color, thickness)
+                print(x,y)
+                # print('to no for')
+                if incremental_error > 0:
+                    y = y + y_increment
+                    incremental_error = incremental_error - 2 * delta_x
 
-            incremental_error = incremental_error + 2 * delta_y
+                incremental_error = incremental_error + 2 * delta_y
+
+        if delta_x > 0:
+            for x in range(initial_x, final_x + 1):
+                self.point(x, y, color, thickness)
+                print(x,y)
+                # print('to no for')
+                if incremental_error > 0:
+                    y = y + y_increment
+                    incremental_error = incremental_error - 2 * delta_x
+
+                incremental_error = incremental_error + 2 * delta_y
 
         #TODO: A impressão por tipo de linha não foi implementada
-        #TODO: Caso delta_x < 0 && delta_y > 0 devemos alterar o ponto inicial com o ponto final, caso contrario a linha
-        #TODO: fica impressa de forma errada.
+        #TODO: Ainda se faz necessário imprimir linhas onde deltaY > deltaX o que inclui linhas verticais
 
     def circle(self, xc, yc, radius, color):
         pass
@@ -98,5 +115,5 @@ class MyGraphics:
 
 a = MyGraphics()
 #a.point(100, 100, 'white', 1)
-a.line(105, 127, 103, 104, 'white', 1,'null')
+a.line(4, 3, 1, 4, 'white', 1,'null')
 a.wait()
